a69d148bb9b5cc929846f4a3281404c3b6036aa1
Breeze <805511939@qq.com>
2018-07-25 11:02:41 +0800

    [*] fix isFetchMode func
    fix isFetchMode func

--- src/Database/CoMySqlClient.php
+++ src/Database/CoMySqlClient.php
@@ -6,6 +6,17 @@ use Swoole\Coroutine\MySQL;
 class CoMySqlClient extends MySQL
 {
     protected $last_used_at;
+    protected $fetch_mode;
+
+    public function connect(array $server_info)
+    {
+        if (array_key_exists('fetch_mode', $server_info)) {
+            $this->fetch_mode = !!$server_info['fetch_mode'];
+        } else {
+            $this->fetch_mode = false;
+        }
+        return parent::connect($server_info);
+    }
 
     public function getLastUsedAt()
     {
@@ -24,10 +35,7 @@ class CoMySqlClient extends MySQL
 
     public function isFetchMode()
     {
-        if (array_key_exists('fetch_mode', $this->serverInfo)) {
-            return $this->serverInfo['fetch_mode'];
-        }
-        return false;
+        return $this->fetch_mode;
     }
 
     public function runSql($query, array $bindings = [])
